d63110d5285b14af12a4b364a8e760de250d3a4f
dragon-fire-fly <ek93@hotmail.co.uk>
2023-04-26 21:56:59 +0200

    Add state to function with arrays

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import SearchBar from "./components/SearchBar";
+import UseStateWithArrays from "./components/UseStateWithArrays";
 
 function App() {
   return <div className="App">
-      <SearchBar />
+      <UseStateWithArrays />
     </div>;
 }
 
